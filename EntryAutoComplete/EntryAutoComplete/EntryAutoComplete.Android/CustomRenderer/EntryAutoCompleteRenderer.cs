33886f907c4049ae4a5df57a0d1d8cdeae20deeb
Krzysztof Stepnikowski <skrzychu7@gmail.com>
2018-09-26 09:41:39 +0200

    master:  Create simple skeleton - creating custom renderer class per platform.

---
+++ EntryAutoComplete/EntryAutoComplete/EntryAutoComplete.Android/CustomRenderer/EntryAutoCompleteRenderer.cs
@@ -0,0 +1,16 @@
+﻿using Android.Content;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+using EntryAutoComplete.Droid.CustomRenderer;
+
+[assembly:ExportRenderer(typeof(EntryAutoComplete.CustomControl.EntryAutoComplete),typeof(EntryAutoCompleteRenderer))]
+namespace EntryAutoComplete.Droid.CustomRenderer
+{
+    public class EntryAutoCompleteRenderer : ViewRenderer
+    {
+        public EntryAutoCompleteRenderer(Context context) : base(context)
+        {
+            
+        }
+    }
+}
---
+++ EntryAutoComplete/EntryAutoComplete/EntryAutoComplete.iOS/CustomRenderer/EntryAutoCompleteRenderer.cs
@@ -0,0 +1,12 @@
+﻿using EntryAutoComplete.iOS.CustomRenderer;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly:ExportRenderer(typeof(EntryAutoComplete.CustomControl.EntryAutoComplete),typeof(EntryAutoCompleteRenderer))]
+namespace EntryAutoComplete.iOS.CustomRenderer
+{
+    public class EntryAutoCompleteRenderer : ViewRenderer
+    {
+       
+    }
+}
