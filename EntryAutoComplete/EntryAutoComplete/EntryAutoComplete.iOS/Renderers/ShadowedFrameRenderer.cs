aec373c21d272882c000cd45e4070cb5f70533b9
Krzysztof Stępnikowski <skrzychu7@gmail.com>
2018-10-17 15:45:49 +0200

    Style support for iOS #16

--- EntryAutoComplete/EntryAutoComplete/EntryAutoComplete.iOS/Renderers/ShadowedFrameRenderer.cs
+++ EntryAutoComplete/EntryAutoComplete/EntryAutoComplete.iOS/Renderers/ShadowedFrameRenderer.cs
@@ -11,36 +11,21 @@ namespace EntryAutoComplete.iOS.Renderers
 {
     public class ShadowedFrameRenderer : FrameRenderer
     {
-        protected override void OnElementChanged(ElementChangedEventArgs<Frame> e)
+        public override void Draw(CGRect rect)
         {
-            base.OnElementChanged(e);
-            if (e.NewElement == null)
-            {
-                return;
-            }
-
+            base.Draw(rect);
             UpdateShadow();
-        }
-
-        protected override void OnElementPropertyChanged(object sender, PropertyChangedEventArgs e)
-        {
-            base.OnElementPropertyChanged(sender, e);
-            if (e.PropertyName.Equals(nameof(ShadowedFrame.ShadowRadius)))
-            {
-                UpdateShadow();
-            }
         }
 
         private void UpdateShadow()
         {
-            var materialFrame = (ShadowedFrame)Element;
-
-            Layer.ShadowRadius = materialFrame.ShadowRadius;
+            Layer.ShadowRadius = 4.0f;
             Layer.ShadowColor = UIColor.Gray.CGColor;
             Layer.ShadowOffset = new CGSize(2, 2);
             Layer.ShadowOpacity = 0.80f;
             Layer.ShadowPath = UIBezierPath.FromRect(Layer.Bounds).CGPath;
             Layer.MasksToBounds = false;
         }
+
     }
 }
